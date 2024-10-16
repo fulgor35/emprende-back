f1810737d77366d84c06cd108bb2c432a097f9c4
fulgor35 <104467382+fulgor35@users.noreply.github.com>
2024-10-15 21:37:05 -0300

    Agregamos package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http")
+
+function requestController() {
+    //lógica de nuestra función
+    console.log("Hola Mundo")
+}
+
+//configurar nuestro servidor
+const server = http.createServer(requestController)
+
+server.listen(4000)
+
+//const http = require("http")
